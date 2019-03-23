bcaa796e1d93bcc1740057fcf67b5bae66315628
Kenniston Arraes Bonfim <kenniston@gmail.com>
2019-03-23 15:29:53 -0300

    guest and host queries

---
+++ graphql-server/src/resolvers/Host.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+var sortByProperty = function (property) {
+    return function (x, y) {
+        return ((x[property] === y[property]) ? 0 : ((x[property] > y[property]) ? 1 : -1))
+    };
+};
+
+const hostServiceConfig = {
+    domain: process.env.HOST_SERVICE_DOMAIN || "host-service",
+    port: process.env.HOST_SERVICE_PORT || "3003"
+}
+
+const Host = {
+
+}
+
+export {
+    Host
+}
--- graphql-server/src/resolvers/Query.js
+++ graphql-server/src/resolvers/Query.js
@@ -11,6 +11,16 @@ const guestServiceConfig = {
     port: process.env.GUEST_SERVICE_PORT || "3002"
 }
 
+const locationServiceConfig = {
+    domain: process.env.LOCATION_SERVICE_DOMAIN || "location-service",
+    port: process.env.LOCATION_SERVICE_PORT || "3001"
+}
+
+const hostServiceConfig = {
+    domain: process.env.HOST_SERVICE_DOMAIN || "host-service",
+    port: process.env.HOST_SERVICE_PORT || "3003"
+}
+
 const Query = {
     getAllGuests: async (_, { orderBy }) => {
         const resp = await axios.get(`http://${guestServiceConfig.domain}:${guestServiceConfig.port}/guest`);
@@ -20,7 +30,31 @@ const Query = {
             data.sort(sortByProperty(orderBy))
         }
         return data;
-    }        
+    },
+
+    getAllHosts: async (_, { orderBy }) => {
+        const resp = await axios.get(`http://${hostServiceConfig.domain}:${hostServiceConfig.port}/host`);
+        var data = resp.data
+        
+        if (orderBy != undefined) {
+            data.sort(sortByProperty(orderBy))
+        }
+        return data;
+    },
+
+    getAllLocations: async (_, { orderBy }) => {
+        const resp = await axios.get(`http://${locationServiceConfig.domain}:${locationServiceConfig.port}/location`);
+        var data = resp.data
+        
+        if (orderBy != undefined) {
+            data.sort(sortByProperty(orderBy))
+        }
+        return data;
+    },
+    guest: async (_, { id }) => {
+        const resp = await axios.get(`http://${guestServiceConfig.domain}:${guestServiceConfig.port}/guest/${id}`);
+        return resp.data;
+    }
 }
 
 export {
